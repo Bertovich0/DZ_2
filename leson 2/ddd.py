b23b44eac6be6b01cd348ac2c205f8f00bd78152
BertO <kuharovivan@gmail.com>
2023-03-12 16:59:24 +0100

    игра

--- leson 2/ddd.py
+++ leson 2/ddd.py
@@ -5,7 +5,7 @@ player1 = Character(name='gaijin', health=100, damage=150,
                     color=Fore.LIGHTRED_EX)
 print(player1)
 
-player2 = Character(name='credit card', health=150, damage=1,
+player2 = Character(name='credit card', health=150, damage=1
                     color=Fore.LIGHTBLUE_EX)
 print(player2)
 
